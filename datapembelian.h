5fff1e761bb04135e4029eeab8cb0b3e6a05ad34
gusmahen <52945813+gusmahen@users.noreply.github.com>
2021-12-28 11:23:04 +0800

    Add files via upload

---
+++ datapembelian.h
@@ -0,0 +1,14 @@
+typedef struct{
+	char nama_pembeli[30];
+	int jumlah_tiket;
+	int kelas_tiket;
+	int harga_tiket;
+	int total_harga;
+	int total_bayar;
+	int kembalian;
+	int no_hari;
+    int no_konser;
+    char nama_hari[255];
+    char nama_konser[255];
+}DataPembelian;
+
