b2bae0eda55eed85655345d3e28f34a4837a8eba
gusmahen <gusde0330@gmail.com>
2022-01-02 15:54:05 +0800

    update
    menambahkan struk pembelian

--- datapembelian.c
+++ datapembelian.c
@@ -1,26 +1,57 @@
 #include "datapembelian.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 void HistoryTransaksi(DataPembelian data[],int jumlah_pembelian){
+    FILE* struk;
+    struk = fopen("struk.txt", "w");
+
+
+
 	int i;
 	int tutup=1;
 	while(tutup==1){
 		for(i=1; i <= jumlah_pembelian; i++ ){
 		    	printf("\t\t==============[ DATA PEMBELIAN ]==============\n");
+		    	fprintf(struk,"==============[ DATA PEMBELIAN ]==============\n");
+
 		    	printf("\t\tKode Tiket Konser      : %d\n", i);
+				fprintf(struk,"Kode Tiket Konser      : %d\n",i);
+
 				printf("\t\tNama                   : %s\n", data[i].nama_pembeli);
+                fprintf(struk,"Nama                   : %s\n", data[i].nama_pembeli);
+
 				printf("\t\tHari Konser Berlangsung: %s\n", data[i].nama_hari);
+				fprintf(struk,"Hari Konser Berlangsung: %s\n", data[i].nama_hari);
+
+
 				printf("\t\tNama Konser            : %s\n", data[i].nama_konser);
+                fprintf(struk,"Nama Konser            : %s\n", data[i].nama_konser);
+
 				printf("\t\tPilihan kelas konser   : %d\n", data[i].kelas_tiket);
+				fprintf(struk,"Pilihan kelas konser   : %d\n", data[i].kelas_tiket);
+
 				printf("\t\tHarga Per-Tiket        : %d\n", data[i].harga_tiket);
+				fprintf(struk,"Harga Per-Tiket        : %d\n", data[i].harga_tiket);
+
 				printf("\t\tJumlah pembelian tiket : %d\n", data[i].jumlah_tiket);
+				fprintf(struk,"Jumlah pembelian tiket : %d\n", data[i].jumlah_tiket);
+
 				printf("\t\tTotal harga            : %d\n", data[i].total_harga);
+				fprintf(struk,"Total harga            : %d\n", data[i].total_harga);
+
 				printf("\t\tUang yang Dibayarkan   : %d\n", data[i].total_bayar);
+				fprintf(struk,"Uang yang Dibayarkan   : %d\n", data[i].total_bayar);
+
 				printf("\t\tKembalian Uang         : %d\n", data[i].kembalian);
+				fprintf(struk,"Kembalian Uang         : %d\n", data[i].kembalian);
+
 				printf("\n\n");
 		    }
 		printf("\t\tUntuk Menutup Halaman Enter '0':");
 		scanf("%d", &tutup);
 	}
+	fclose(struk);
 }
 
 void PencarianHistory(DataPembelian data[],int jumlah_pembelian){
